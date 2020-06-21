a82f9105d3f1bbefe1175370706ecc9b6173a03f
Sh031224 <1cktmdgh2@gmail.com>
2020-06-21 12:54:41 +0900

    [FIX] ormconfig

---
+++ backend/ormconfig.js
@@ -0,0 +1,18 @@
+module.exports = {
+  type: "mysql",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
+  synchronize: true,
+  logging: false,
+  entities: ["src/entity/**/*.ts"],
+  migrations: ["src/migration/**/*.ts"],
+  subscribers: ["src/subscriber/**/*.ts"],
+  cli: {
+    entitiesDir: "src/entity",
+    migrationsDir: "src/migration",
+    subscribersDir: "src/subscriber"
+  }
+};
