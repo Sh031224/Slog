fa395498198482bf282c02ef25701c0d068f6f81
Sh031224 <1cktmdgh2@gmail.com>
2020-06-22 20:40:08 +0900

    [ADD] create category

--- backend/src/api/v1/index.ts
+++ backend/src/api/v1/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import category from "./category";
+import auth from "./auth";
 
 const router = Router();
 
 router.use("/category", category);
+router.use("/auth", auth);
 
 export default router;
