b4a5aa6f8a756dc01b8d68bc43c6c44f9f996cfa
Sh031224 <1cktmdgh2@gmail.com>
2020-06-22 20:40:24 +0900

    [ADD] category joi

---
+++ backend/src/lib/validation/category.ts
@@ -0,0 +1,30 @@
+import * as Joi from "joi";
+import validate from "./validate";
+import { Request, Response } from "express";
+
+export const validateCreate = (req: Request, res: Response): boolean => {
+  const schema = Joi.object().keys({
+    name: Joi.string().max(40).required()
+  });
+
+  return validate(req, res, schema);
+};
+
+export const validateModify = (req: Request, res: Response): boolean => {
+  const schema = Joi.object().keys({
+    name: Joi.string().max(40).required()
+  });
+
+  return validate(req, res, schema);
+};
+
+export const validateModifyOrderNumber = (
+  req: Request,
+  res: Response
+): boolean => {
+  const schema = Joi.object().keys({
+    order_number: Joi.array().items(Joi.number()).required()
+  });
+
+  return validate(req, res, schema);
+};
