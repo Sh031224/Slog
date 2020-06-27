bd67d12a9e125111a84414b71d94c856d4c4ede4
Sh031224 <1cktmdgh2@gmail.com>
2020-06-27 22:47:39 +0900

    [ADD] addCategory.ts

--- backend/src/api/v1/category/index.ts
+++ backend/src/api/v1/category/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import getCategories from "./category.ctrl/getCategories";
 import getCategory from "./category.ctrl/getCategory";
+import addCategory from "./category.ctrl/addCategory";
 
 const router = Router();
 
 router.get("/", getCategories);
 router.get("/:idx", getCategory);
+router.post("/", addCategory);
 
 export default router;
