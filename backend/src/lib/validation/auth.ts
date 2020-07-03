3420e4a96105d32866aaa7933fe472cd77c7c5ea
Sh031224 <1cktmdgh2@gmail.com>
2020-07-03 22:49:53 +0900

    [ADD] login validate

---
+++ backend/src/lib/validation/auth.ts
@@ -0,0 +1,11 @@
+import * as Joi from "joi";
+import validate from "./validate";
+import { Request, Response } from "express";
+
+export const validateLogin = (req: Request, res: Response): boolean => {
+  const schema = Joi.object().keys({
+    access_token: Joi.string().required()
+  });
+
+  return validate(req, res, schema);
+};
