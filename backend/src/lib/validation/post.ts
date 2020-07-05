1c50fef231b5b4af0dedf12b059dc2b561fd1bc4
Sh031224 <1cktmdgh2@gmail.com>
2020-07-05 18:19:12 +0900

    [ADD] validate post

---
+++ backend/src/lib/validation/post.ts
@@ -0,0 +1,25 @@
+import * as Joi from "joi";
+import validate from "./validate";
+import { Request, Response } from "express";
+
+export const validateCreate = (req: Request, res: Response): boolean => {
+  const schema = Joi.object().keys({
+    title: Joi.string().min(1).max(50).required(),
+    content: Joi.string().required(),
+    category_idx: Joi.number().integer().required(),
+    thumbnail: Joi.string().max(800).allow(null)
+  });
+
+  return validate(req, res, schema);
+};
+
+export const validateModify = (req: Request, res: Response): boolean => {
+  const schema = Joi.object().keys({
+    title: Joi.string().min(1).max(50),
+    content: Joi.string(),
+    category_idx: Joi.number().integer(),
+    thumbnail: Joi.string().max(800)
+  });
+
+  return validate(req, res, schema);
+};
