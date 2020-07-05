e1e861886cd04bbbdd0398989a653e7edbc9879e
Sh031224 <1cktmdgh2@gmail.com>
2020-07-05 18:18:02 +0900

    [ADD] post

--- backend/src/api/v1/index.ts
+++ backend/src/api/v1/index.ts
@@ -3,6 +3,7 @@ import category from "./category";
 import auth from "./auth";
 import notice from "./notice";
 import profile from "./profile";
+import post from "./post";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use("/category", category);
 router.use("/auth", auth);
 router.use("/notice", notice);
 router.use("/profile", profile);
+router.use("/post", post);
 
 export default router;
---
+++ backend/src/entity/Post.ts
@@ -0,0 +1,57 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  BaseEntity,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn
+} from "typeorm";
+import Category from "./Category";
+
+@Entity("post")
+export default class Post extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  idx: number;
+
+  @Column({
+    length: 255,
+    nullable: false
+  })
+  title: string;
+
+  @Column("text", {
+    nullable: false
+  })
+  content: string;
+
+  @Column({
+    nullable: false,
+    default: 0
+  })
+  view: number;
+
+  @ManyToOne((type) => Category, { onDelete: "SET NULL" })
+  @JoinColumn({ name: "fk_category_idx" })
+  category: Category;
+
+  @Column({
+    nullable: true
+  })
+  fk_category_idx: number;
+
+  @Column({
+    length: 1000,
+    nullable: false
+  })
+  thumbnail: string;
+
+  @Column("timestamp")
+  @CreateDateColumn()
+  created_at: Date;
+
+  @Column("timestamptz")
+  @UpdateDateColumn()
+  updated_at: Date;
+}
