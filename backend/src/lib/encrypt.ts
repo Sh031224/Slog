919b57ecfee7709234ed219c6b78b5e5783d4978
Sh031224 <1cktmdgh2@gmail.com>
2020-07-07 09:42:09 +0900

    [ADD] crypto

---
+++ backend/src/lib/encrypt.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import * as crypto from "crypto";
+
+const { ENCRYPT_SECRET } = process.env;
+
+export default (text: string): string => {
+  const encrypted = crypto
+    .createHmac("sha256", process.env.CRYPTO_SECRET)
+    .update(text)
+    .digest("hex");
+
+  return encrypted;
+};
