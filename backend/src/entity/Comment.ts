d1cf0ce57d4543deb1602c7d6ce259f48be48b69
Sh031224 <1cktmdgh2@gmail.com>
2020-07-08 11:05:47 +0900

    [ADD] Private Comment

---
+++ backend/src/entity/Comment.ts
@@ -0,0 +1,69 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  BaseEntity,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn
+} from "typeorm";
+import User from "./User";
+import Post from "./Post";
+
+@Entity("comment")
+export default class Comment extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  idx: number;
+
+  @Column("text", {
+    nullable: false
+  })
+  content: string;
+
+  @Column({
+    nullable: false,
+    default: false
+  })
+  has_replies: boolean;
+
+  @Column({
+    default: false,
+    nullable: false
+  })
+  is_private: boolean;
+
+  @ManyToOne((type) => User, (user) => user.comments, { onDelete: "SET NULL" })
+  @JoinColumn([
+    { name: "fk_user_idx", referencedColumnName: "idx" },
+    { name: "fk_user_name", referencedColumnName: "name" }
+  ])
+  user: User;
+
+  @Column({
+    nullable: true
+  })
+  fk_user_idx: number;
+
+  @Column({
+    nullable: true
+  })
+  fk_user_name: string;
+
+  @ManyToOne((type) => Post, { onDelete: "CASCADE" })
+  @JoinColumn({ name: "fk_post_idx" })
+  post: Post;
+
+  @Column({
+    nullable: true
+  })
+  fk_post_idx: number;
+
+  @Column("timestampz")
+  @CreateDateColumn()
+  created_at: Date;
+
+  @Column("timestamptz")
+  @UpdateDateColumn()
+  updated_at: Date;
+}
