d19ffed65995aaf3b3ea604ae0d5470bb85d0b61
Sh031224 <1cktmdgh2@gmail.com>
2020-07-08 11:06:59 +0900

    [ADD] Temp Post

--- backend/src/api/v1/post/index.ts
+++ backend/src/api/v1/post/index.ts
@@ -6,15 +6,19 @@ import getPost from "./post.ctrl/getPost";
 import searchPosts from "./post.ctrl/searchPosts";
 import deletePost from "./post.ctrl/deletePost";
 import modifyPost from "./post.ctrl/modifyPost";
+import getTempPosts from "./post.ctrl/getTempPosts";
+import createTempPost from "./post.ctrl/createTempPost";
 
 const router = Router();
 
+router.get("/temp", authMiddleware.admin, getTempPosts);
 router.get("/search", searchPosts);
 router.put("/:idx", authMiddleware.admin, modifyPost);
 
+router.post("/temp", authMiddleware.admin, createTempPost);
 router.post("/", authMiddleware.admin, createPost);
 router.get("/", getPosts);
-router.get("/:idx", getPost);
+router.get("/:idx", authMiddleware.guest, getPost);
 
 router.delete("/:idx", authMiddleware.admin, deletePost);
 
--- backend/src/api/v1/post/post.ctrl/createPost.ts
+++ backend/src/api/v1/post/post.ctrl/createPost.ts
@@ -18,37 +18,42 @@ export default async (req: Request, res: Response) => {
   const body: RequestBody = req.body;
 
   try {
-    const categoryRepo = getRepository(Category);
-    const category: Category = await categoryRepo.findOne({
-      where: {
-        idx: body.category_idx
-      }
-    });
-
-    if (!category) {
-      logger.yellow("[POST] 글 생성 카테고리 없음.");
-      res.status(404).json({
-        message: "해당 카테고리 없음."
-      });
-      return;
-    }
-
     const postRepo = getRepository(Post);
     const post = new Post();
+    if (body.category_idx) {
+      const categoryRepo = getRepository(Category);
+      const category: Category = await categoryRepo.findOne({
+        where: {
+          idx: body.category_idx
+        }
+      });
+
+      if (!category) {
+        logger.yellow("[POST] 글 생성 카테고리 없음.");
+        res.status(404).json({
+          status: 404,
+          message: "해당 카테고리 없음."
+        });
+        return;
+      }
+      post.category = category;
+    }
 
     post.title = body.title;
     post.content = body.content;
     post.thumbnail = body.thumbnail;
-    post.category = category;
+
     await postRepo.save(post);
 
     logger.green("[POST] 글 생성 성공.");
     res.status(200).json({
+      status: 200,
       message: "글 생성 성공."
     });
   } catch (err) {
     logger.red("POST] 글 생성 서버 오류.", err.message);
     res.status(500).json({
+      status: 500,
       message: "서버 오류."
     });
   }
