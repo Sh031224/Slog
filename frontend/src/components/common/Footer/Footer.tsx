d25ce77c76210c9ad1777e6d946494a636a91a44
Sh031224 <1cktmdgh2@gmail.com>
2020-07-13 19:23:47 +0900

    [ADD] Footer

--- frontend/src/components/common/Footer/Footer.tsx
+++ frontend/src/components/common/Footer/Footer.tsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./Footer.scss";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <a href="https://github.com/Sh031224" target="_blank">
+          © 2020. Sh031224. All rights reserved.
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
