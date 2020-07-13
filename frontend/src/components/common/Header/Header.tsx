5fe7d22c1a26719abb02aee38b1f681abeaf94eb
Sh031224 <1cktmdgh2@gmail.com>
2020-07-13 19:24:10 +0900

    [ADD] Header

--- frontend/src/components/common/Header/Header.tsx
+++ frontend/src/components/common/Header/Header.tsx
@@ -3,15 +3,40 @@ import { Link } from "react-router-dom";
 import "./Header.scss";
 import logo from "../../../assets/images/logo.svg";
 import HeaderSearch from "./HeaderSearch";
+import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
+import { appId } from "../../../config/index.json";
+import {
+  ReactFacebookFailureResponse,
+  ReactFacebookLoginInfo
+} from "react-facebook-login";
 
 interface HeaderProps {
   searchEl: MutableRefObject<any>;
   inputEl: MutableRefObject<any>;
   search: string;
   setSearch: Dispatch<React.SetStateAction<string>>;
+  login: boolean;
+  tryLogin: (res: FacebookLoginInfo | FacebookFailureResponse) => void;
+  tryLogout: () => void;
 }
 
-const Header = ({ searchEl, inputEl, search, setSearch }: HeaderProps) => {
+interface FacebookLoginInfo extends ReactFacebookLoginInfo {
+  accessToken: string;
+}
+
+interface FacebookFailureResponse extends ReactFacebookFailureResponse {
+  accessToken?: string;
+}
+
+const Header = ({
+  searchEl,
+  inputEl,
+  search,
+  setSearch,
+  login,
+  tryLogout,
+  tryLogin
+}: HeaderProps) => {
   return (
     <header className="header">
       <div className="header-container">
@@ -26,9 +51,32 @@ const Header = ({ searchEl, inputEl, search, setSearch }: HeaderProps) => {
               searchEl={searchEl}
               inputEl={inputEl}
             />
-            <Link to="/login">
-              <span className="header-container-main-util-login">로그인</span>
-            </Link>
+            {login ? (
+              <span
+                onClick={tryLogout}
+                className="header-container-main-util-login"
+              >
+                로그아웃
+              </span>
+            ) : (
+              <FacebookLogin
+                appId={appId}
+                fields="name,email"
+                callback={tryLogin}
+                render={(renderProps: any) => {
+                  return (
+                    <div
+                      className="login-container-button"
+                      onClick={renderProps.onClick}
+                    >
+                      <span className="header-container-main-util-login">
+                        로그인
+                      </span>
+                    </div>
+                  );
+                }}
+              />
+            )}
           </div>
         </div>
       </div>
--- frontend/src/containers/Header/HeaderContainer.tsx
+++ frontend/src/containers/Header/HeaderContainer.tsx
@@ -1,11 +1,62 @@
-import React, { useRef, useState } from "react";
+import { inject, observer } from "mobx-react";
+import React, { useEffect, useRef, useState } from "react";
 import Header from "../../components/common/Header";
+import LoginStore from "../../stores/LoginStore";
+import {
+  ReactFacebookFailureResponse,
+  ReactFacebookLoginInfo
+} from "react-facebook-login";
+import { useCookies } from "react-cookie";
 
-const HeaderContainer = () => {
+interface HeaderContainerProps {
+  store?: StoreType;
+}
+
+interface StoreType {
+  LoginStore: LoginStore;
+}
+
+interface FacebookLoginInfo extends ReactFacebookLoginInfo {
+  accessToken: string;
+}
+
+interface FacebookFailureResponse extends ReactFacebookFailureResponse {
+  accessToken?: string;
+}
+
+const HeaderContainer = ({ store }: HeaderContainerProps) => {
+  const { login, handleLoginChange, handleLogin } = store!.LoginStore;
   const searchEl = useRef<HTMLElement>(null);
   const inputEl = useRef<HTMLElement>(null);
 
   const [search, setSearch] = useState("");
+
+  const [cookies, setCookie, removeCookie] = useCookies(["access_token"]);
+
+  const tryLogin = (res: FacebookLoginInfo | FacebookFailureResponse) => {
+    handleLogin(res.accessToken!)
+      .then((response: any) => {
+        const today = new Date();
+        today.setDate(today.getDate() + 7);
+        setCookie("access_token", response.data.access_token, {
+          expires: today
+        });
+      })
+      .catch((err: Error) => {
+        alert("[Error] 로그인에 실패하였습니다.");
+      });
+  };
+
+  const tryLogout = () => {
+    removeCookie("access_token");
+    handleLoginChange(false);
+  };
+
+  useEffect(() => {
+    if (cookies.access_token) {
+      handleLoginChange(true);
+    }
+  }, []);
 
   return (
     <Header
@@ -13,8 +64,11 @@ const HeaderContainer = () => {
       setSearch={setSearch}
       searchEl={searchEl}
       inputEl={inputEl}
+      login={login}
+      tryLogin={tryLogin}
+      tryLogout={tryLogout}
     />
   );
 };
 
-export default HeaderContainer;
+export default inject("store")(observer(HeaderContainer));
