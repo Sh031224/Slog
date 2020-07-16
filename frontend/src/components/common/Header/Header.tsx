520f97803067e74c70d4cd8028d3ab0f594cb054
Sh031224 <1cktmdgh2@gmail.com>
2020-07-16 20:10:55 +0900

    [FIX] Header Design

--- frontend/src/components/common/Header/Header.tsx
+++ frontend/src/components/common/Header/Header.tsx
@@ -1,10 +1,13 @@
 import React, { Dispatch, MutableRefObject } from "react";
 import { Link } from "react-router-dom";
+import HeadRoom from "react-headroom";
 import "./Header.scss";
 import logo from "../../../assets/images/logo.svg";
+import ScrollToTop from "react-scroll-to-top";
 import HeaderSearch from "./HeaderSearch";
 import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
 import { appId } from "../../../config/index.json";
+import { GrLinkTop } from "react-icons/gr";
 import {
   ReactFacebookFailureResponse,
   ReactFacebookLoginInfo
@@ -38,49 +41,58 @@ const Header = ({
   tryLogin
 }: HeaderProps) => {
   return (
-    <header className="header">
-      <div className="header-container">
-        <div className="header-container-main">
-          <Link to="/">
-            <img className="header-container-main-logo" src={logo} alt="logo" />
-          </Link>
-          <div className="header-container-main-util">
-            <HeaderSearch
-              search={search}
-              setSearch={setSearch}
-              searchEl={searchEl}
-              inputEl={inputEl}
-            />
-            {login ? (
-              <span
-                onClick={tryLogout}
-                className="header-container-main-util-login"
-              >
-                로그아웃
-              </span>
-            ) : (
-              <FacebookLogin
-                appId={appId}
-                fields="name,email"
-                callback={tryLogin}
-                render={(renderProps: any) => {
-                  return (
-                    <div
-                      className="login-container-button"
-                      onClick={renderProps.onClick}
-                    >
-                      <span className="header-container-main-util-login">
-                        로그인
-                      </span>
-                    </div>
-                  );
-                }}
-              />
-            )}
+    <>
+      <HeadRoom>
+        <header className="header">
+          <div className="header-container">
+            <div className="header-container-main">
+              <Link to="/">
+                <img
+                  className="header-container-main-logo"
+                  src={logo}
+                  alt="logo"
+                />
+              </Link>
+              <div className="header-container-main-util">
+                <HeaderSearch
+                  search={search}
+                  setSearch={setSearch}
+                  searchEl={searchEl}
+                  inputEl={inputEl}
+                />
+                {login ? (
+                  <span
+                    onClick={tryLogout}
+                    className="header-container-main-util-login"
+                  >
+                    로그아웃
+                  </span>
+                ) : (
+                  <FacebookLogin
+                    appId={appId}
+                    fields="name,email"
+                    callback={tryLogin}
+                    render={(renderProps: any) => {
+                      return (
+                        <div
+                          className="login-container-button"
+                          onClick={renderProps.onClick}
+                        >
+                          <span className="header-container-main-util-login">
+                            로그인
+                          </span>
+                        </div>
+                      );
+                    }}
+                  />
+                )}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </header>
+        </header>
+      </HeadRoom>
+      <ScrollToTop smooth component={<GrLinkTop />} />
+    </>
   );
 };
 
