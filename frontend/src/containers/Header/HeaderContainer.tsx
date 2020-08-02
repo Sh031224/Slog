75a1320a72e045151cbf9f2ab94ff69001d7ab3f
Sh031224 <1cktmdgh2@gmail.com>
2020-08-02 18:14:15 +0900

    [ADD] FCM

--- frontend/src/containers/Header/HeaderContainer.tsx
+++ frontend/src/containers/Header/HeaderContainer.tsx
@@ -10,6 +10,7 @@ import {
 import Swal from "sweetalert2";
 import { useCookies } from "react-cookie";
 import { useHistory } from "react-router-dom";
+import firebase from "firebase";
 
 interface HeaderContainerProps {
   store?: StoreType;
@@ -33,7 +34,8 @@ const HeaderContainer = ({ store }: HeaderContainerProps) => {
     haldleAdminFalse,
     login,
     handleLoginChange,
-    handleLogin
+    handleLogin,
+    handleFcm
   } = store!.UserStore;
   const searchEl = useRef<HTMLElement>(null);
   const inputEl = useRef<HTMLElement>(null);
@@ -55,7 +57,7 @@ const HeaderContainer = ({ store }: HeaderContainerProps) => {
         });
         axios.defaults.headers.common["access_token"] = cookies.access_token;
         Swal.fire("환영합니다!", "로그인에 성공하였습니다.", "success");
-        handleUser(response.data.access_token);
+        handleAll(response.data.access_token);
       })
       .catch((err: Error) => {
         haldleAdminFalse();
@@ -86,11 +88,36 @@ const HeaderContainer = ({ store }: HeaderContainerProps) => {
     }
   }, [search]);
 
+  const getFcmToken = useCallback(async () => {
+    const messaging = firebase.messaging();
+
+    await messaging.requestPermission().then(() => {
+      messaging.getToken().then((res: string) => {
+        handleFcm(res);
+      });
+    });
+  }, []);
+
+  const handleAll = async (access_token: string) => {
+    await handleUser(access_token);
+    axios.defaults.headers.common["access_token"] = cookies.access_token;
+    if (Notification.permission === "granted") {
+      getFcmToken();
+    } else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then(
+        (permission: NotificationPermission) => {
+          if (permission === "granted") {
+            getFcmToken();
+          }
+        }
+      );
+    }
+  };
+
   useEffect(() => {
     if (cookies.access_token !== undefined) {
       handleLoginChange(true);
-      handleUser(cookies.access_token);
-      axios.defaults.headers.common["access_token"] = cookies.access_token;
+      handleAll(cookies.access_token);
     }
   }, []);
 
