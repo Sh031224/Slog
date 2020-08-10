441dcd53bed1bcbc2842ec21f4c18a724359d538
Sh031224 <1cktmdgh2@gmail.com>
2020-08-10 18:19:18 +0900

    [ADD] validateAdmin

--- frontend/src/containers/Create/CreateContainer.tsx
+++ frontend/src/containers/Create/CreateContainer.tsx
@@ -7,6 +7,7 @@ import UserStore from "../../stores/UserStore";
 import { NotificationManager } from "react-notifications";
 import { inject, observer } from "mobx-react";
 import Create from "../../components/Create";
+import { useHistory } from "react-router-dom";
 
 interface CreateContainerProps {
   store?: StoreType;
@@ -18,6 +19,19 @@ interface StoreType {
   PostStore: PostStore;
 }
 
+interface GetProfileResponse {
+  status: number;
+  message: string;
+  data: {
+    user: {
+      idx: number;
+      name: string;
+      is_admin: boolean;
+      created_at: Date;
+    };
+  };
+}
+
 const CreateContainer = ({ store }: CreateContainerProps) => {
   const { categoryList, handleCategoryList } = store!.CategoryStore;
   const { handleUser, admin, login, handleLoginChange } = store!.UserStore;
@@ -26,24 +40,37 @@ const CreateContainer = ({ store }: CreateContainerProps) => {
 
   const [source, setSource] = useState<string>("");
 
-  useEffect(() => {
+  const history = useHistory();
+
+  const validateAdmin = () => {
     try {
       axios.defaults.headers.common["access_token"] = cookies.access_token;
       if (cookies.access_token !== undefined) {
         handleLoginChange(true);
-        handleUser(cookies.access_token).catch((err) => {
-          if (err.message === "401") {
-            removeCookie("access_token", { path: "/" });
-            handleLoginChange(false);
-            axios.defaults.headers.common["access_token"] = "";
-          }
-        });
+        handleUser(cookies.access_token)
+          .then((res: GetProfileResponse) => {
+            if (!res.data.user.is_admin) {
+              history.push("/");
+            }
+          })
+          .catch((err) => {
+            if (err.message === "401") {
+              removeCookie("access_token", { path: "/" });
+              handleLoginChange(false);
+              axios.defaults.headers.common["access_token"] = "";
+            }
+          });
       } else {
+        history.push("/");
         handleLoginChange(false);
       }
     } catch (err) {
       NotificationManager.error("오류가 발생하였습니다.", "Error");
     }
+  };
+
+  useEffect(() => {
+    validateAdmin();
   }, [login]);
 
   useEffect(() => {
