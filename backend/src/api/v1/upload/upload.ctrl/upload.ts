f5e4a7eac717a38d3b3bf6dd94e87b8feb8e1ac5
Sh031224 <1cktmdgh2@gmail.com>
2020-08-12 03:55:09 +0900

    [FIX] image upload route

--- backend/src/api/v1/upload/upload.ctrl/upload.ts
+++ backend/src/api/v1/upload/upload.ctrl/upload.ts
@@ -1,40 +1,14 @@
 import { Response } from "express";
-import * as fs from "fs";
 import logger from "../../../../lib/logger";
-import { validateUpload } from "../../../../lib/validation/upload";
+import generateURL from "../../../../lib/util/generateURL";
 
 export default async (req: any, res: Response) => {
-  if (!validateUpload(req, res)) return;
-
-  const { post_idx } = req.body;
-
   try {
     const reqFiles = req.files;
     const files: string[] = [];
 
     reqFiles.forEach((reqFile: any) => {
-      const oldPath = `public/tmp/${reqFile.filename}`;
-      const newPath = `public/${post_idx}/${reqFile.filename}`;
-      if (!fs.existsSync(`public/${post_idx}`)) {
-        fs.mkdirSync(`public/${post_idx}`);
-      }
-
-      files.push(reqFile.filename);
-
-      fs.rename(
-        oldPath,
-        newPath,
-        (err: NodeJS.ErrnoException) =>
-          void {
-            if(err: NodeJS.ErrnoException) {
-              logger.red("[POST] 파일 업로드 서버 오류.");
-              res.status(500).json({
-                status: 500,
-                message: "서버 오류."
-              });
-            }
-          }
-      );
+      files.push(generateURL(req, reqFile.filename));
     });
 
     logger.green("[POST] 파일 업로드 성공.");
@@ -46,7 +20,7 @@ export default async (req: any, res: Response) => {
       }
     });
   } catch (err) {
-    logger.red("[POST] 파일 업로드 서버 오류.");
+    logger.red("[POST] 파일 업로드 서버 오류.", err.message);
     res.status(500).json({
       status: 500,
       message: "서버 오류."
