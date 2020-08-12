c79b4c5c469b9efaaf1f2b14956f68283e8253c8
Sh031224 <1cktmdgh2@gmail.com>
2020-08-12 16:12:01 +0900

    [ADD] HandlePostHeader

---
+++ frontend/src/components/common/HandlePost/HandlePostHeader/HandlePostHeader.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HandlePostHeader.scss";
+
+const HandlePostHeader = () => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default HandlePostHeader;
