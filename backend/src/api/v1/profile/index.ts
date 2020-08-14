eca45c062c38ebc9444f5be3d3d43b04018dfb19
Sh031224 <1cktmdgh2@gmail.com>
2020-08-15 03:18:20 +0900

    [ADD] AdminProfile

--- backend/src/api/v1/profile/index.ts
+++ backend/src/api/v1/profile/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import authMiddleware from "../../../lib/middleware/auth";
 import getProfile from "./profile.ctrl/getProfile";
 import getMyProfile from "./profile.ctrl/getMyProfile";
+import getAdminProfile from "./profile.ctrl/getAdminProfile";
 
 const router = Router();
 
 router.get("/my", authMiddleware.user, getMyProfile);
+router.get("/admin", getAdminProfile);
 router.get("/:idx", getProfile);
 
 export default router;
