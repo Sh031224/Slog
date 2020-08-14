83186c81f2d6c1f8bcc40fce466f8df14149ca52
Sh031224 <1cktmdgh2@gmail.com>
2020-08-14 19:01:24 +0900

    [FIX] Design

--- frontend/src/components/common/HandlePost/HandlePostCategory/HandlePostCategory.tsx
+++ frontend/src/components/common/HandlePost/HandlePostCategory/HandlePostCategory.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./HandlePostCategory.scss";
 
 interface HandlePostCategoryProps {
   categoryList: CategoryType[];
@@ -17,18 +18,17 @@ const HandlePostCategory = ({
   setCategoryIdx
 }: HandlePostCategoryProps) => {
   return (
-    <>
+    <div className="handle-post-category">
       <select
+        required
         value={categoryIdx}
-        onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
-          setCategoryIdx(Number(e.target.value))
-        }
+        onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+          setCategoryIdx(Number(e.target.value));
+        }}
         name="category"
         id="category_idx"
       >
-        <option value={-1} selected>
-          카테고리를 선택해주세요.
-        </option>
+        <option value="">카테고리를 선택해주세요.</option>
         {categoryList.map((category: CategoryType) => {
           return (
             <option value={category.idx} key={category.idx}>
@@ -37,7 +37,7 @@ const HandlePostCategory = ({
           );
         })}
       </select>
-    </>
+    </div>
   );
 };
 
