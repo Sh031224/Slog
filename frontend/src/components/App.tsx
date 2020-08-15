b542daa5777e0a64826aa3c0df4d4f8a0a8a589c
Sh031224 <1cktmdgh2@gmail.com>
2020-08-16 00:44:35 +0900

    .

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,25 +1,11 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
+import MainPage from "../pages/MainPage";
+import PostPage from "../pages/PostPage";
 import firebase from "firebase/app";
+import HandlePage from "../pages/HandlePage";
 import { option } from "../config/firebase.json";
-<<<<<<< HEAD
 import NotfoundPage from "../pages/NotfoundPage";
-=======
-import loadable from "@loadable/component";
-
-const MainPage = loadable(() =>
-  import(/* webpackChunkName: "Header" */ "../pages/MainPage")
-);
-const PostPage = loadable(() =>
-  import(/* webpackChunkName: "Header" */ "../pages/PostPage")
-);
-const HandlePage = loadable(() =>
-  import(/* webpackChunkName: "Header" */ "../pages/HandlePage")
-);
-const NotfoundPage = loadable(() =>
-  import(/* webpackChunkName: "Header" */ "../pages/NotfoundPage")
-);
->>>>>>> 3cb1b1f2e41c3812a8e4138bb716201311f262ae
 
 firebase.initializeApp(option);
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,39 +1,25 @@
 import React from "react";
-import { hydrate } from "react-dom";
+import ReactDOM from "react-dom";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "mobx-react";
 import { BrowserRouter } from "react-router-dom";
 import stores from "./stores";
 import "./util/util.scss";
-// import { HelmetProvider } from "react-helmet-async";
-import { loadableReady } from "@loadable/component";
+import { HelmetProvider } from "react-helmet-async";
 
-<<<<<<< HEAD
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
   <HelmetProvider>
-=======
-loadableReady(() => {
-  const rootElement = document.getElementById("root");
-  hydrate(
->>>>>>> 3cb1b1f2e41c3812a8e4138bb716201311f262ae
     <Provider store={stores}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-<<<<<<< HEAD
     </Provider>
   </HelmetProvider>,
   rootElement
 );
-=======
-    </Provider>,
-    rootElement
-  );
-});
->>>>>>> 3cb1b1f2e41c3812a8e4138bb716201311f262ae
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
