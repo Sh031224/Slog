0042c51e9d2c521aa67fbfdbfae6f1f48560afe3
Sh031224 <1cktmdgh2@gmail.com>
2020-08-17 18:16:02 +0900

    [FIX] Style Error

--- frontend/src/react-app-env.d.ts
+++ frontend/src/react-app-env.d.ts
@@ -2,8 +2,4 @@
 declare module "react-facebook-login/dist/facebook-login-render-props";
 declare module "react-notifications";
 declare module "react-useinterval";
-<<<<<<< HEAD
 declare module "*.png";
-=======
-declare module "@loadable/component";
->>>>>>> 3cb1b1f2e41c3812a8e4138bb716201311f262ae
