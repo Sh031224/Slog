9b80fc62d20ff1db7c71c2768580ab688fbcce2d
Sh031224 <1cktmdgh2@gmail.com>
2020-08-26 20:10:08 +0900

    [FIX] MarkDown

--- frontend/src/components/Markdown/Markdown.tsx
+++ frontend/src/components/Markdown/Markdown.tsx
@@ -7,10 +7,48 @@ interface MarkdownProps {
   children: string;
 }
 
+const markedH1 = ({ children, ...props }: { children: string }) => {
+  return <h1 id={children.toString().replace(/\ /g, "-")}>{children}</h1>;
+};
+
+const markedH2 = ({ children, ...props }: { children: string }) => {
+  return <h2 id={children.toString().replace(/\ /g, "-")}>{children}</h2>;
+};
+
+const markedH3 = ({ children, ...props }: { children: string }) => {
+  return <h3 id={children.toString().replace(/\ /g, "-")}>{children}</h3>;
+};
+
+const markedH4 = ({ children, ...props }: { children: string }) => {
+  return <h4 id={children.toString().replace(/\ /g, "-")}>{children}</h4>;
+};
+
+const markedH5 = ({ children, ...props }: { children: string }) => {
+  return <h5 id={children.toString().replace(/\ /g, "-")}>{children}</h5>;
+};
+
+const markedH6 = ({ children, ...props }: { children: string }) => {
+  return <h6 id={children.toString().replace(/\ /g, "-")}>{children}</h6>;
+};
+
 const Markdown = ({ children }: MarkdownProps) => {
   return (
     <div className="markdown-code">
-      <MarkDown options={{ forceBlock: true }}>{children}</MarkDown>
+      <MarkDown
+        options={{
+          overrides: {
+            h1: markedH1,
+            h2: markedH2,
+            h3: markedH3,
+            h4: markedH4,
+            h5: markedH5,
+            h6: markedH6
+          },
+          forceBlock: true
+        }}
+      >
+        {children}
+      </MarkDown>
     </div>
   );
 };
