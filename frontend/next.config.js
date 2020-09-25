97903b31be11d26a24d524435679fe36f9eaa4d4
Sh031224 <1cktmdgh2@gmail.com>
2020-09-25 22:04:38 +0900

    [ADD] bundle moudle

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,8 +2,26 @@ const withImages = require("next-images");
 const withPlugins = require("next-compose-plugins");
 const withSass = require("@zeit/next-sass");
 const withCss = require("@zeit/next-css");
+const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
 
 module.exports = withPlugins([
+  [
+    withBundleAnalyzer,
+    {
+      analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+      analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+      bundleAnalyzerConfig: {
+        server: {
+          analyzerMode: "static",
+          reportFilename: "../bundles/server.html"
+        },
+        browser: {
+          analyzerMode: "static",
+          reportFilename: "../bundles/client.html"
+        }
+      }
+    }
+  ],
   [
     withSass,
     {
