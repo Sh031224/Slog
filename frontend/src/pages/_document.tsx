d95bc32a9f3af36c9d4d1d47219626c644a14f08
Sh031224 <1cktmdgh2@gmail.com>
2020-09-25 23:57:10 +0900

    [UPDATE] favicon

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -11,11 +11,11 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
           <meta name="theme-color" content="#000000" />
-          <link rel="apple-touch-icon" href="/logo192.png" />
-          <link rel="shortcut icon" href="/favicon.ico" />
-          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/images/logo192.png" />
+          <link rel="shortcut icon" href="/images/favicon.ico" />
+          <link rel="manifest" href="/images/manifest.json" />
         </Head>
         <body>
           <Main />
