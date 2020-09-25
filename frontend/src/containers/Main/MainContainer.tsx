8974c02fb6be73765ab547da46ad4f16592c55e0
Sh031224 <1cktmdgh2@gmail.com>
2020-09-26 00:35:29 +0900

    [FIX] Head

--- frontend/src/containers/Main/MainContainer.tsx
+++ frontend/src/containers/Main/MainContainer.tsx
@@ -198,34 +198,25 @@ const MainContainer = ({ store }: MainContainerProps) => {
         <meta
           name="description"
           content="많은 사람들에게 유용한 정보를 제공하기 위해 제작한 Slog입니다."
-          data-react-helmet="true"
         />
-        <meta name="og:title" content="Slog" data-react-helmet="true" />
+        <meta name="og:title" content="Slog" />
         <meta
           property="og:description"
           content="많은 사람들에게 유용한 정보를 제공하기 위해 제작한 Slog입니다."
-          data-react-helmet="true"
         />
-        <meta
-          property="og:url"
-          content="https://slog.website/"
-          data-react-helmet="true"
-        />
+        <meta property="og:url" content="https://slog.website/" />
         <meta
           property="og:image"
           content="https://data.slog.website/public/op_logo.png"
-          data-react-helmet="true"
         />
-        <meta name="twitter:title" content="Slog" data-react-helmet="true" />
+        <meta name="twitter:title" content="Slog" />
         <meta
           property="twitter:description"
           content="많은 사람들에게 유용한 정보를 제공하기 위해 제작한 Slog입니다."
-          data-react-helmet="true"
         />
         <meta
           property="twitter:image"
           content="https://data.slog.website/public/op_logo.png"
-          data-react-helmet="true"
         />
       </Head>
       <Main
