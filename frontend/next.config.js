4b0a5d35b617e81ff64c580606df8908971d6632
Sh031224 <1cktmdgh2@gmail.com>
2020-09-27 13:44:27 +0900

    [FIX] next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,27 +1,20 @@
-const withImages = require("next-images");
-const withPlugins = require("next-compose-plugins");
+const withCSS = require("@zeit/next-css");
 const withSass = require("@zeit/next-sass");
-const withCss = require("@zeit/next-css");
 
-module.exports = withPlugins([
-  [
-    withSass,
-    {
-      cssModules: true,
-      cssLoaderOptions: {
-        importLoaders: 1,
-        localIdentName: "[local]",
-        url: false
-      }
+module.exports = withCSS(
+  withSass({
+    webpack(config, options) {
+      config.module.rules.push({
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 100000
+          }
+        }
+      });
+
+      return config;
     }
-  ],
-  [
-    withCss,
-    {
-      cssLoaderOptions: {
-        url: false
-      }
-    }
-  ],
-  [withImages]
-]);
+  })
+);
