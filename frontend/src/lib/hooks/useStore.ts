aca891c3eddd796b5bf54bd8d60461cbc2d0a32f
Sh031224 <1cktmdgh2@gmail.com>
2020-12-03 15:57:36 +0900

    Refactor: add useStore

--- frontend/src/lib/hooks/useStore.ts
+++ frontend/src/lib/hooks/useStore.ts
@@ -2,6 +2,8 @@ import React from "react";
 import { MobXProviderContext } from "mobx-react";
 import StoreType from "../../types/StoreType";
 
-export default (): StoreType => {
+const useStore = (): StoreType => {
   return React.useContext(MobXProviderContext) as StoreType;
 };
+
+export default useStore;
