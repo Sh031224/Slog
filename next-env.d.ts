7ad1f9c0e3909266770a4996ef4ee0b19122c9d0
Sh031224 <1cktmdgh2@gmail.com>
2021-04-26 20:45:59 +0900

    Fix: Next Config

--- next-env.d.ts
+++ next-env.d.ts
@@ -5,5 +5,13 @@ declare module "redux-wait-for-action";
 declare module "react-facebook-login/dist/facebook-login-render-props";
 declare module "react-notifications";
 declare module "react-useinterval";
-declare module "*.png";
-declare module "*.svg";
+declare module "*.png" {
+  const value: any;
+  export default value;
+}
+declare module "*.svg" {
+  import React = require("react");
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
