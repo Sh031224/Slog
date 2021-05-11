c92b4077e4513d0af4a5abbaf8044b937fd1de9f
Sh031224 <1cktmdgh2@gmail.com>
2021-05-11 16:45:59 +0900

    Add: Post & Handle test code

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.tsx"],
+  moduleNameMapper: {
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/__mocks__/fileMock.js"
+  },
   moduleDirectories: ["node_modules", "src"]
 };
