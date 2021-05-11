3c1ca9000dcf4ac44dc472000f851f3a100736b1
Sh031224 <1cktmdgh2@gmail.com>
2021-05-11 15:10:23 +0900

    Add: setup test

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.tsx"],
+  moduleDirectories: ["node_modules", "src"]
+};
---
+++ src/setupTests.tsx
@@ -0,0 +1,29 @@
+import { ThemeProvider } from "styled-components";
+import { render } from "@testing-library/react";
+import { lightTheme } from "style/theme";
+import { store } from "stores";
+import { Provider } from "react-redux";
+import { NotificationContainer } from "react-notifications";
+
+const Providers = ({ children }) => {
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={lightTheme}>
+        <NotificationContainer />
+        {children}
+      </ThemeProvider>
+    </Provider>
+  );
+};
+
+const customRender = (ui, options = {}) => render(ui, { wrapper: Providers, ...options });
+
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import "@testing-library/jest-dom/extend-expect";
+
+(window as any).scrollTo = () => {};
+
+export { customRender as render };
