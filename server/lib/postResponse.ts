e479181773a1598d702cac8e7e979a6569ea15f2
Sh031224 <1cktmdgh2@gmail.com>
2021-06-11 09:18:40 +0900

    Add: server.json

--- server/lib/postResponse.ts
+++ server/lib/postResponse.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from "axios";
 import { IPost } from "interface/IPost";
 import { GetPostsResponse } from "interface/IResponse";
 
-const SERVER = require("../../src/config/index.json").server;
+const SERVER = require("../../src/config/server.json").server;
 
 const postResponse = async (limit?: number): Promise<IPost[]> => {
   const { data }: AxiosResponse<GetPostsResponse> = await axios.get(
