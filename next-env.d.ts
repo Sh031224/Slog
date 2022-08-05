5fcb698e84eff10fffa60c09a7a17d8726226135
Sh031224 <1cktmdgh2@gmail.com>
2022-08-05 10:37:48 +0900

    feature: node / next / react version up

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,15 +1,5 @@
 /// <reference types="next" />
-/// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
 
-declare module "react-facebook-login/dist/facebook-login-render-props";
-declare module "react-notifications";
-declare module "*.png" {
-  const value: any;
-  export default value;
-}
-declare module "*.svg" {
-  import React = require("react");
-  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-  const src: string;
-  export default src;
-}
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
