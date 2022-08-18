68d60f28a74570937b22620e103e236e93082210
Sh031224 <1cktmdgh2@gmail.com>
2022-08-18 17:54:35 +0900

    server-package: version fix

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express on Vercel");
+  return "";
 });
 
 AppDataSource.initialize()
