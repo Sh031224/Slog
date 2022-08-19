275f61a12503bb729e42517d07d3f611c907deb2
Sh031224 <1cktmdgh2@gmail.com>
2022-08-20 03:47:34 +0900

    fix: setup test

--- apps/web/jest.config.js
+++ apps/web/jest.config.js
@@ -1,16 +1,12 @@
-module.exports = {
-  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.tsx"],
-  moduleNameMapper: {
-    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/__mocks__/fileMock.js"
-  },
-  moduleDirectories: ["node_modules", "src"],
-  collectCoverage: true,
-  coverageReporters: ["text", "cobertura"],
-  coverageThreshold: {
-    global: {
-      lines: 70
-    }
-  }
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./"
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ["./src/setupTests.ts"],
+  testEnvironment: "jest-environment-jsdom"
 };
+
+module.exports = createJestConfig(customJestConfig);
