20fa3ab3e12d764d1e5a5404a0fb3fc7442046e0
Sh031224 <1cktmdgh2@gmail.com>
2022-08-23 00:45:01 +0900

    test-server: add test code services, repository

---
+++ apps/server/src/services/__tests__/image-service.test.ts
@@ -0,0 +1,30 @@
+import BadRequestError from "../../models/error/bad-request-error";
+import ImageService from "../image-service";
+
+const upload = jest.fn();
+
+jest.mock("../../lib/google-cloud.ts", () => {
+  return function () {
+    return { upload };
+  };
+});
+
+describe("image-service.ts", () => {
+  const imageService = new ImageService();
+
+  describe("upload", () => {
+    it("if req.file is undefined, throw error", async () => {
+      await expect(imageService.upload({} as any)).rejects.toThrowError(
+        new BadRequestError("image is required")
+      );
+    });
+
+    it("if req.file is not undefined, upload google cloud", async () => {
+      const file = "file";
+
+      await imageService.upload({ file } as any);
+
+      expect(upload).toBeCalledWith(file);
+    });
+  });
+});
