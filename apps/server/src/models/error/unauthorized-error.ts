8c9cdf3523c67a0a0fe903a001db054569e5d509
Sh031224 <1cktmdgh2@gmail.com>
2022-08-22 21:41:54 +0900

    test-server: add ignore line

--- apps/server/src/models/error/unauthorized-error.ts
+++ apps/server/src/models/error/unauthorized-error.ts
@@ -2,6 +2,7 @@ import httpStatus from "http-status";
 import CustomError from ".";
 
 export default class UnauthorizedError extends CustomError {
+  /* istanbul ignore next */
   constructor(message: string) {
     super(message);
 
