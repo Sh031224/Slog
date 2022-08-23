1f4c42add0841a5bae56806edff180a84e51d1b5
Sh031224 <1cktmdgh2@gmail.com>
2022-08-24 08:23:40 +0900

    add: res.cookie for mock response

--- apps/server/src/test-utils.ts
+++ apps/server/src/test-utils.ts
@@ -2,6 +2,7 @@ type MockResponse = {
   status?: jest.Mock<any>;
   json?: jest.Mock<any>;
   end?: jest.Mock<any>;
+  cookie?: jest.Mock<any>;
 };
 
 export const getMockResponse = () => {
@@ -9,5 +10,6 @@ export const getMockResponse = () => {
   res.status = jest.fn().mockReturnValue(res);
   res.json = jest.fn().mockReturnValue(res);
   res.end = jest.fn().mockReturnValue(res);
+  res.cookie = jest.fn().mockReturnValue(res);
   return res;
 };
