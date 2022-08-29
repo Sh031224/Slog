6e014da4986aaacb2cecd8bb62677451bc4596f6
Sh031224 <1cktmdgh2@gmail.com>
2022-08-29 12:04:01 +0900

    feature: add category services

---
+++ apps/server/src/validator/category-validator.ts
@@ -0,0 +1,43 @@
+import { Request } from "express";
+import Joi from "joi";
+
+import Validator from ".";
+import BadRequestError from "../models/error/bad-request-error";
+
+export default class CategoryValidator extends Validator {
+  constructor() {
+    super();
+  }
+
+  create = (req: Request) => {
+    const schema = Joi.object().keys({
+      name: Joi.string().max(40).required()
+    });
+
+    this.validate(req, schema);
+  };
+
+  update = (req: Request) => {
+    if (isNaN(Number(req.params.idx))) throw new BadRequestError("idx is required");
+
+    const schema = Joi.object().keys({
+      name: Joi.string().max(40).required()
+    });
+
+    this.validate(req, schema);
+  };
+
+  updateOrderNumber = (req: Request) => {
+    if (isNaN(Number(req.params.idx))) throw new BadRequestError("idx is required");
+
+    const schema = Joi.object().keys({
+      orderNumber: Joi.number().min(1).required()
+    });
+
+    this.validate(req, schema);
+  };
+
+  delete = (req: Request) => {
+    if (isNaN(Number(req.params.idx))) throw new BadRequestError("idx is required");
+  };
+}
