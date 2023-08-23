65325b43a919358a5baf7de2e1908df4b9de03c7
Sh031224 <1cktmdgh2@gmail.com>
2023-08-23 18:42:02 +0900

    design: update post not found page

--- src/app/explore/@posts/not-found.tsx
+++ src/app/explore/@posts/not-found.tsx
@@ -1,8 +1,8 @@
 export default function PostsNotFound() {
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <p className="text-2xl font-semibold tracking-tight">
-        😭 There are no posts that match the criteria.
+      <p className="text-xl font-semibold tracking-tight text-muted-foreground">
+        There are no posts that match the criteria.
       </p>
     </div>
   );
