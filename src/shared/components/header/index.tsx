ee9137d723522d5b7aa217faa3c57e28977048fc
Sh031224 <1cktmdgh2@gmail.com>
2023-08-24 18:22:21 +0900

    design: category sticky ui change

--- src/shared/components/header/index.tsx
+++ src/shared/components/header/index.tsx
@@ -14,7 +14,7 @@ export async function Header() {
   const user = (await auth())?.user;
 
   return (
-    <header className="supports-backdrop-blur:bg-background/60 sticky top-0 z-30 w-full border-b bg-background/95 backdrop-blur">
+    <header className="sticky top-0 z-30 w-full border-b bg-background/95 backdrop-blur">
       <div className="container flex h-14 items-center justify-between px-4 sm:px-8">
         <Link href="/" className="flex items-center gap-2">
           <ActivityIcon size="24" />
