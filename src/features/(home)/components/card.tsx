adc27797fdff891d1cac587f7aacf163c1ae28d2
Sh031224 <1cktmdgh2@gmail.com>
2023-08-25 19:21:39 +0900

    feature: add post detail path

--- src/features/(home)/components/card.tsx
+++ src/features/(home)/components/card.tsx
@@ -1,5 +1,6 @@
 import type { Post } from '@prisma/client';
 import Image from 'next/image';
+import Link from 'next/link';
 import { forwardRef } from 'react';
 
 type Props = {
@@ -7,33 +8,35 @@ type Props = {
 };
 
 const Card = forwardRef<HTMLDivElement, Props>(({ data }, ref) => (
-  <div className="border-none bg-card text-card-foreground" ref={ref}>
-    <div className="relative pt-[52%]">
-      {!!data.thumbnail ? (
-        <Image
-          className="absolute top-0 rounded border object-cover dark:brightness-90"
-          src={data.thumbnail}
-          alt={data.title}
-          fill
-          priority
-        />
-      ) : (
-        <div className="absolute top-0 h-full w-full rounded border bg-muted-foreground" />
-      )}
+  <Link href={`/post/${data.id}`}>
+    <div className="border-none bg-card text-card-foreground" ref={ref}>
+      <div className="relative pt-[52%]">
+        {!!data.thumbnail ? (
+          <Image
+            className="absolute top-0 rounded border object-cover dark:brightness-90"
+            src={data.thumbnail}
+            alt={data.title}
+            fill
+            priority
+          />
+        ) : (
+          <div className="absolute top-0 h-full w-full rounded border bg-muted-foreground" />
+        )}
+      </div>
+
+      <div className="mt-4 flex flex-col space-y-1.5 px-2">
+        <h3 className="line-clamp-2 text-lg font-semibold leading-none tracking-tight">
+          {data.title}
+        </h3>
+
+        <p className="line-clamp-3 text-sm text-muted-foreground">
+          {data.description}
+        </p>
+
+        <p className="text-xs text-muted-foreground/50">7일 전</p>
+      </div>
     </div>
-
-    <div className="mt-4 flex flex-col space-y-1.5 px-2">
-      <h3 className="line-clamp-2 text-lg font-semibold leading-none tracking-tight">
-        {data.title}
-      </h3>
-
-      <p className="line-clamp-3 text-sm text-muted-foreground">
-        {data.description}
-      </p>
-
-      <p className="text-xs text-muted-foreground/50">7일 전</p>
-    </div>
-  </div>
+  </Link>
 ));
 
 Card.displayName = 'Card';
