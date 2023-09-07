f3525b36b462574bac346e4ccf0abfd292c4aa60
차승호 <1cktmdgh2@gmail.com>
2023-09-08 01:25:15 +0900

    feat: sign-in page runtime change

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 
 import SignIn from '@/features/(auth)/sign-in';
 
+export const runtime = 'edge';
+
 export const metadata: Metadata = {
   title: 'Sign In',
   description: 'Sign in to your account'
