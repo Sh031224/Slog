13ebb68c5d6da53016b331c17bdfa3b5028c2d73
Sh031224 <1cktmdgh2@gmail.com>
2023-10-25 17:58:40 +0900

    Revert "fix: change button text"
    This reverts commit 2c24fad86d41c2e3924360c1046efe4c0172d739.

--- src/features/(auth)/sign-in/index.tsx
+++ src/features/(auth)/sign-in/index.tsx
@@ -5,7 +5,7 @@ export default function SignIn() {
     <div className="mx-auto flex w-full flex-col items-center justify-center space-y-6 sm:w-[500px]">
       <div className="flex flex-col space-y-2 text-center sm:space-y-4">
         <h1 className="text-2xl font-semibold tracking-tight sm:text-4xl">
-          Sign In
+          로그인/회원가입
         </h1>
 
         <p className="text-sm text-muted-foreground sm:text-lg">
